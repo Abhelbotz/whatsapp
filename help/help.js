498d8062e42d9f095dae67bcc3a729c9b88596f4
Abhelbotz <indoshop030721@gmail.com>
2022-01-23 07:10:45 +0700

    whatsapp

---
+++ help/help.js
@@ -0,0 +1,404 @@
+const help = (prefix) => {
+    return `❏ *INGFO*
+│◪Var : Termux
+│◪Prefix : ${prefix}
+│◪Host : Phone
+└◪› 𝑨𝑩𝑯𝑬𝑳-𝑮𝑯𝑶𝒁𝑯𝑰𝑵-𝑩𝑶𝑻
+⎮〄${prefix}help
+⎮〄${prefix}donate
+⎮〄${prefix}sticker
+⎮〄${prefix}stickerwm
+⎮〄${prefix}broadcast
+⎮〄${prefix}hidetag
+⎮〄${prefix}tagstick
+⎮〄${prefix}clearall
+│
+└◪› 「 Islami 」
+⎮〄${prefix}listsurah
+⎮〄${prefix}asmaulhusna
+⎮〄${prefix}alquran no_surah
+⎮〄${prefix}alquran no_surah/no_ayat
+⎮〄${prefix}alquran no_surah/no_ayat1-no_ayat2
+⎮〄${prefix}alquranaudio no_surah
+⎮〄${prefix}alquranaudio no_surah/no_ayat
+⎮〄${prefix}kisahnabi nama_nabi
+⎮〄${prefix}jadwalsholat daerah
+│
+└◪› 「 Downloader 」
+⎮〄${prefix}ytsearch query
+⎮〄${prefix}ytplay query
+⎮〄${prefix}ytmp3 url_video
+⎮〄${prefix}ytmp4 url_video
+⎮〄${prefix}tiktoknowm url_video
+⎮〄${prefix}tiktokmusic url_video
+⎮〄${prefix}igdl url_post
+⎮〄${prefix}fbdl url_video
+⎮〄${prefix}jooxplay query
+⎮〄${prefix}spotify url_music
+⎮〄${prefix}spotifysearch query
+⎮〄${prefix}pinterest query
+⎮〄${prefix}pinterestdl url_pinterest
+⎮〄${prefix}pixiv query
+⎮〄${prefix}pixivdl url_pixiv
+⎮〄${prefix}zippyshare url_zippyshare
+⎮〄${prefix}telesticker url_pack
+│
+└◪› 「 Movie & Story 」
+⎮〄${prefix}drakorongoing
+⎮〄${prefix}lk21 query
+⎮〄${prefix}wattpad url_wattpad
+⎮〄${prefix}wattpadsearch query
+⎮〄${prefix}cerpen
+⎮〄${prefix}ceritahoror
+│
+└◪› 「 Searching 」
+⎮〄${prefix}shopee query
+⎮〄${prefix}google query
+⎮〄${prefix}gimage query
+⎮〄${prefix}gimage2 query
+⎮〄${prefix}konachan query
+⎮〄${prefix}playstore query
+⎮〄${prefix}stickerwa query
+⎮〄${prefix}wallpapersearch query
+⎮〄${prefix}wallpapersearch2 query
+│
+└◪› 「 Random Text 」
+⎮〄${prefix}quotes
+⎮〄${prefix}quotesdilan
+⎮〄${prefix}quotesanime
+⎮〄${prefix}quotesimage
+⎮〄${prefix}faktaunik
+⎮〄${prefix}katabijak
+⎮〄${prefix}pantun
+⎮〄${prefix}bucin
+⎮〄${prefix}randomnama
+│
+└◪› 「 AniManga 」
+⎮〄${prefix}wait
+⎮〄${prefix}manga query
+⎮〄${prefix}anime query
+⎮〄${prefix}character query
+⎮〄${prefix}kusonime url_kusonime
+⎮〄${prefix}kusonimesearch query
+⎮〄${prefix}otakudesu url_otakudesu
+⎮〄${prefix}otakudesusearch query
+⎮〄${prefix}nhentai kode_bom
+⎮〄${prefix}nhentaipdf kode_bom
+⎮〄${prefix}nhentaisearch query
+⎮〄${prefix}nekopoi url
+⎮〄${prefix}nekopoisearch query
+│
+└◪› 「 Information 」
+⎮〄${prefix}kbbi
+⎮〄${prefix}brainly2
+⎮〄${prefix}jarak
+⎮〄${prefix}urbandictionary
+⎮〄${prefix}jadwaltv channel
+⎮〄${prefix}jadwaltvnow
+⎮〄${prefix}jadwalbola
+⎮〄${prefix}qrreader
+⎮〄${prefix}heroml hero_name
+⎮〄${prefix}mlstalk id/server
+⎮〄${prefix}genshin character
+⎮〄${prefix}wikipedia query
+⎮〄${prefix}translate kode_negara text
+⎮〄${prefix}brainly query
+⎮〄${prefix}newsinfo
+⎮〄${prefix}cnnindonesia
+⎮〄${prefix}cnnnasional
+⎮〄${prefix}cnninternasional
+⎮〄${prefix}infogempa
+⎮〄${prefix}lirik query
+⎮〄${prefix}cuaca daerah
+⎮〄${prefix}kodepos query
+⎮〄${prefix}indbeasiswa
+⎮〄${prefix}hoax
+⎮〄${prefix}nsfwcheck
+⎮〄${prefix}ocr
+│
+└◪› 「 Entertainment 」
+⎮〄${prefix}asupan
+⎮〄${prefix}wancak
+⎮〄${prefix}tebakgambar
+⎮〄${prefix}canceltebakgambar
+⎮〄${prefix}akinator
+⎮〄${prefix}cancelakinator
+│
+└◪› 「 Creator 」
+⎮〄${prefix}ttp text
+⎮〄${prefix}ttp2 text
+⎮〄${prefix}ttp3 text
+⎮〄${prefix}ttp4 text
+⎮〄${prefix}attp text
+⎮〄${prefix}smoji emoji
+⎮〄${prefix}fakedonald text
+⎮〄${prefix}ktpmaker
+│
+└◪› 「 Primbon 」
+⎮〄${prefix}artinama name
+⎮〄${prefix}jodoh name1 & name2 
+⎮〄${prefix}weton tanggal bulan tahun
+⎮〄${prefix}jadian tanggal bulan tahun
+⎮〄${prefix}tebakumur name
+│
+└◪› 「 Other 」
+⎮〄${prefix}ssweb link
+⎮〄${prefix}ssweb2 link
+⎮〄${prefix}shortlink link
+⎮〄${prefix}spamsms nomor
+│
+└◪› 「 Text Pro Me 」
+⎮〄${prefix}blackpink text
+⎮〄${prefix}neon text
+⎮〄${prefix}greenneon text
+⎮〄${prefix}advanceglow text
+⎮〄${prefix}futureneon text
+⎮〄${prefix}sandwriting text
+⎮〄${prefix}sandsummer text
+⎮〄${prefix}sandengraved text
+⎮〄${prefix}metaldark text
+⎮〄${prefix}neonlight text
+⎮〄${prefix}holographic text
+⎮〄${prefix}text1917 text
+⎮〄${prefix}minion text
+⎮〄${prefix}deluxesilver text
+⎮〄${prefix}newyearcard text
+⎮〄${prefix}bloodfrosted text
+⎮〄${prefix}halloween text
+⎮〄${prefix}jokerlogo text
+⎮〄${prefix}fireworksparkle text
+⎮〄${prefix}natureleaves text
+⎮〄${prefix}bokeh text
+⎮〄${prefix}toxic text
+⎮〄${prefix}strawberry text
+⎮〄${prefix}box3d text
+⎮〄${prefix}roadwarning text
+⎮〄${prefix}breakwall text
+⎮〄${prefix}icecold text
+⎮〄${prefix}luxury text
+⎮〄${prefix}cloud text
+⎮〄${prefix}summersand text
+⎮〄${prefix}horrorblood text
+⎮〄${prefix}thunder text
+⎮〄${prefix}pornhub text1 text2
+⎮〄${prefix}glitch text1 text2
+⎮〄${prefix}avenger text1 text2
+⎮〄${prefix}space text1 text2
+⎮〄${prefix}ninjalogo text1 text2
+⎮〄${prefix}marvelstudio text1 text2
+⎮〄${prefix}lionlogo text1 text2
+⎮〄${prefix}wolflogo text1 text2
+⎮〄${prefix}steel3d text1 text2
+⎮〄${prefix}wallgravity text1 text2
+│
+└◪› 「 Photo Oxy 」
+⎮〄${prefix}shadow text
+⎮〄${prefix}cup text
+⎮〄${prefix}cup1 text
+⎮〄${prefix}romance text
+⎮〄${prefix}smoke text
+⎮〄${prefix}burnpaper text
+⎮〄${prefix}lovemessage text
+⎮〄${prefix}undergrass text
+⎮〄${prefix}love text
+⎮〄${prefix}coffe text
+⎮〄${prefix}woodheart text
+⎮〄${prefix}woodenboard text
+⎮〄${prefix}summer3d text
+⎮〄${prefix}wolfmetal text
+⎮〄${prefix}nature3d text
+⎮〄${prefix}underwater text
+⎮〄${prefix}golderrose text
+⎮〄${prefix}summernature text
+⎮〄${prefix}letterleaves text
+⎮〄${prefix}glowingneon text
+⎮〄${prefix}fallleaves text
+⎮〄${prefix}flamming text
+⎮〄${prefix}harrypotter text
+⎮〄${prefix}carvedwood text
+⎮〄${prefix}tiktok text1 text2
+⎮〄${prefix}arcade8bit text1 text2
+⎮〄${prefix}battlefield4 text1 text2
+⎮〄${prefix}pubg text1 text2
+│
+└◪› 「 Ephoto 360 」
+⎮〄${prefix}wetglass text
+⎮〄${prefix}multicolor3d text
+⎮〄${prefix}watercolor text
+⎮〄${prefix}luxurygold text
+⎮〄${prefix}galaxywallpaper text
+⎮〄${prefix}lighttext text
+⎮〄${prefix}beautifulflower text
+⎮〄${prefix}puppycute text
+⎮〄${prefix}royaltext text
+⎮〄${prefix}heartshaped text
+⎮〄${prefix}birthdaycake text
+⎮〄${prefix}galaxystyle text
+⎮〄${prefix}hologram3d text
+⎮〄${prefix}greenneon text
+⎮〄${prefix}glossychrome text
+⎮〄${prefix}greenbush text
+⎮〄${prefix}metallogo text
+⎮〄${prefix}noeltext text
+⎮〄${prefix}glittergold text
+⎮〄${prefix}textcake text
+⎮〄${prefix}starsnight text
+⎮〄${prefix}wooden3d text
+⎮〄${prefix}textbyname text
+⎮〄${prefix}writegalacy text
+⎮〄${prefix}galaxybat text
+⎮〄${prefix}snow3d text
+⎮〄${prefix}birthdayday text
+⎮〄${prefix}goldplaybutton text
+⎮〄${prefix}silverplaybutton text
+⎮〄${prefix}freefire text
+│
+└◪› 「 Random Image  」
+⎮〄${prefix}art
+⎮〄${prefix}bts
+⎮〄${prefix}exo
+⎮〄${prefix}elf
+⎮〄${prefix}loli
+⎮〄${prefix}neko
+⎮〄${prefix}waifu
+⎮〄${prefix}shota
+⎮〄${prefix}husbu
+⎮〄${prefix}sagiri
+⎮〄${prefix}shinobu
+⎮〄${prefix}megumin
+⎮〄${prefix}wallnime
+⎮〄${prefix}chiisaihentai
+⎮〄${prefix}trap
+⎮〄${prefix}blowjob
+⎮〄${prefix}yaoi
+⎮〄${prefix}ecchi
+⎮〄${prefix}hentai
+⎮〄${prefix}ahegao
+⎮〄${prefix}hololewd
+⎮〄${prefix}sideoppai
+⎮〄${prefix}animefeets
+⎮〄${prefix}animebooty
+⎮〄${prefix}animethighss
+⎮〄${prefix}hentaiparadise
+⎮〄${prefix}animearmpits
+⎮〄${prefix}hentaifemdom
+⎮〄${prefix}lewdanimegirls
+⎮〄${prefix}biganimetiddies
+⎮〄${prefix}animebellybutton
+⎮〄${prefix}hentai4everyone
+⎮〄${prefix}bj
+⎮〄${prefix}ero
+⎮〄${prefix}cum
+⎮〄${prefix}feet
+⎮〄${prefix}yuri
+⎮〄${prefix}trap
+⎮〄${prefix}lewd
+⎮〄${prefix}feed
+⎮〄${prefix}eron
+⎮〄${prefix}solo
+⎮〄${prefix}gasm
+⎮〄${prefix}poke
+⎮〄${prefix}anal
+⎮〄${prefix}holo
+⎮〄${prefix}tits
+⎮〄${prefix}kuni
+⎮〄${prefix}kiss
+⎮〄${prefix}erok
+⎮〄${prefix}smug
+⎮〄${prefix}baka
+⎮〄${prefix}solog
+⎮〄${prefix}feetg
+⎮〄${prefix}lewdk
+⎮〄${prefix}waifu
+⎮〄${prefix}pussy
+⎮〄${prefix}femdom
+⎮〄${prefix}cuddle
+⎮〄${prefix}hentai
+⎮〄${prefix}eroyuri
+⎮〄${prefix}cum_jpg
+⎮〄${prefix}blowjob
+⎮〄${prefix}erofeet
+⎮〄${prefix}holoero
+⎮〄${prefix}classic
+⎮〄${prefix}erokemo
+⎮〄${prefix}fox_girl
+⎮〄${prefix}futanari
+⎮〄${prefix}lewdkemo
+⎮〄${prefix}wallpaper
+⎮〄${prefix}pussy_jpg
+⎮〄${prefix}kemonomimi
+⎮〄${prefix}nsfw_avatar
+⎮〄${prefix}ngif
+⎮〄${prefix}nsfw_neko_gif
+⎮〄${prefix}random_hentai_gif
+└◪›.  ( TQTO )
+│CREDIT BY : 𝑨𝑩𝑯𝑬𝑳-𝑮𝑯𝑶𝒁𝑯𝑰𝑵
+│Sadistic_bots
+│all of you
+╰───「 Sadistic_bots 」`
+}
+exports.help = help
+
+const donate = (sender) => {
+    return `Isi punya situ ae ngabs`
+}
+exports.donate = donate
+
+
+const bahasa = () => {
+    return `
+List Bahasa :
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
